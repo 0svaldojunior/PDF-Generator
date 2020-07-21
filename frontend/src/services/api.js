09cb76a7a2c474ad1041a421c5a000c302f5fcbc
0svaldojunior <osvaldo.rodrigues@acad.pucrs.br>
2020-07-21 19:14:50 -0300

    Production on Server-AWS

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Criação da conexão com o banco de dados gerando a api de serviços e selecionando a URL
 // onde ele se encontra
 const api = axios.create({
-    baseURL: 'http://192.168.0.101:3000'
+    baseURL: 'http://localhost:3000'
 });
 
 export default api;
